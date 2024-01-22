c1210641a6282d543515e43ea847a439f6a29fef
BrandonJJB <brandonjosejulio@gmail.com>
2024-01-22 00:00:14 -0500

    boton volver home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,33 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
 
-  slides=[
-    {
-      title: "Bienvenido a Foreman!",
-      description:"Explora un nuevo nivel de conveniencia en la compra de boletos.",
-      img:"../../assets/img/img1.jpg",
-      help_text: "Swipe para continuar",
-    },
-    {
-      title: "Tu Portal Personalizado de Boletos con Foreman",
-      description:"Inicia tu viaje con Foreman, tu destino para boletos personalizados.",
-      img:"../../assets/img/img2.jpg",
-      help_text: "Swipe para continuar",
-    },
-    {
-      title: "Comodidad en la Palma de tu Mano con Foreman",
-      description:"¡Salta las filas y accede a tus eventos preferidos de manera fácil y rápida con Foreman!",
-      img:"../../assets/img/img3.jpg",
-      help_text: "Swipe para continuar",
-    }
-    
-  ]
-  constructor() {}
+ 
+  constructor( private router:Router) {}
 
+  goToIntro(){
+    console.log("go to intro");
+    this.router.navigateByUrl('/intro');
+}
 }
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: 'intro.page.html',
+  styleUrls: ['intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slides=[
+    {
+      title: "Bienvenido a Foreman!",
+      description:"Explora un nuevo nivel de conveniencia en la compra de boletos.",
+      img:"../../assets/img/img1.jpg",
+      help_text: "Swipe para continuar",
+    },
+    {
+      title: "Tu Portal Personalizado de Boletos con Foreman",
+      description:"Inicia tu viaje con Foreman, tu destino para boletos personalizados.",
+      img:"../../assets/img/img2.jpg",
+      help_text: "Swipe para continuar",
+    },
+    {
+      title: "Comodidad en la Palma de tu Mano con Foreman",
+      description:"¡Salta las filas y accede a tus eventos preferidos de manera fácil y rápida con Foreman!",
+      img:"../../assets/img/img3.jpg",
+      help_text: "Swipe para continuar",
+      class:"slide-3"
+    }
+    
+  ]
+  constructor( private router:Router) {}
+
+  goToHome(){
+    console.log("go to home");
+    this.router.navigateByUrl('/home');
+}
+
+  ngOnInit() {
+  }
+
+}
