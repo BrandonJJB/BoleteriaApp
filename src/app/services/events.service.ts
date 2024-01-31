47d477850540e55ac2a186b3641b5011423c4b77
BrandonJJB <brandonjosejulio@gmail.com>
2024-01-30 23:43:04 -0500

    1/30/2024

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EventsService {
-
+urlServer="http://localhost:3000";
   constructor() { }
+
+getEvents(){
+return fetch(`${this.urlServer}/events`).then(
+  response=>response.json()
+);
+}
+
 }
